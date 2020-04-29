c3be90ee120887edccd6be21de756624ef9e7e96
Anurag Saini <anuragsaini@Anurags-MBP.lan>
2020-04-30 01:13:52 +0530

    updated primeNumbers.java file

--- 3. Coding/PrimeNumbers.java
+++ 3. Coding/PrimeNumbers.java
@@ -8,8 +8,8 @@ import java.util.Scanner;
 public class PrimeNumbers {
 
 	public static void main(String args[]) {
-		int countShouldBeLessThanTwo = 0;
-		int primeNumberIndex = 1;
+		byte countShouldBeLessThanTwo = 0;
+		long primeNumberIndex = 1;
 		int totalPrimeNumbers = 0;
 		boolean invalidInput = true;
 		while (invalidInput) {
@@ -17,34 +17,32 @@ public class PrimeNumbers {
 				System.out.print("How many prime numbers you want to see: ");
 				Scanner scanner = new Scanner(System.in);
 				totalPrimeNumbers = scanner.nextInt();
-				if (totalPrimeNumbers != 0) {
+				if (totalPrimeNumbers > 0) {
 					invalidInput = false;
-				}
-				else {
+				} else {
 					System.out.println("Please enter only natural number...\n");
 				}
 			} catch (InputMismatchException e) {
-				System.out.println("Please enter only natural number...\n");
+				System.out.println("Please enter a natural number between range (1 to 2147483647) \n");
 			}
 		}
-		Instant start = Instant.now();
-		System.out.println("First " + totalPrimeNumbers + " Prime Numbers Are:-");
-		for (int i = 2; primeNumberIndex <= totalPrimeNumbers; i++) {
-			for (int j = i; j > 1; j--) {
+		
+		System.out.println("First " + totalPrimeNumbers + " Prime numbers are:-");
+		for (long i = 2; primeNumberIndex <= totalPrimeNumbers; i++) {
+			for (long j = i; j > 1; j--) {
 				if (i % j == 0) {
 					countShouldBeLessThanTwo++;
 				}
 			}
 			if (countShouldBeLessThanTwo < 2) {
-				System.out.println(primeNumberIndex + "\tprime number is: " + i);
-				primeNumberIndex++;
+				if (i > 1 && primeNumberIndex > 0) {
+					System.out.println(primeNumberIndex + "\tprime number is: " + i);
+					primeNumberIndex++;
+				} else {
+					break;
+				}
 			}
 			countShouldBeLessThanTwo = 0;
 		}
-		Instant finish = Instant.now();
-		long timeElapsed = Duration.between(start, finish).toMillis();
-
-		System.out.println("\n\nTotal time taken by program to find the " + primeNumberIndex + " prime numbers, are "
-				+ timeElapsed + " milli seconds");
 	}
 }
